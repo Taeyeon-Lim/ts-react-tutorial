e102988ed85ee08730d57c5bdec52325e2d7c31e
Teayeon-Lim <wingofyeon@naver.com>
2021-11-04 17:22:03 +0900

    타입스크립트에서 Context 다루기

--- src/ReducerSample.tsx
+++ src/ReducerSample.tsx
@@ -1,54 +1,13 @@
-// Counter.tsx 보다 좀 더 복잡한 Reducer를 사용하는 예제
-import React, { useReducer } from 'react';
-
-type Color = 'red' | 'orange' | 'yellow';
-
-type State = {
-  count: number;
-  text: string;
-  color: Color;
-  isGood: boolean;
-};
-
-type Action =
-  | { type: 'SET_COUNT'; count: number }
-  | { type: 'SET_TEXT'; text: string }
-  | { type: 'SET_COLOR'; color: Color }
-  | { type: 'TOGGLE_GOOD' };
-
-// 파라미터로 [State, Action]타입을 받아서 반드시 State 타입의 데이터를 반환
-function reducer(state: State, action: Action): State {
-  switch (action.type) {
-    case 'SET_COUNT':
-      // 'SET_COUNT' 타입 내의 action 안에 [ count ]가 있으므로 자동 완성됨
-      return { ...state, count: action.count };
-    case 'SET_TEXT':
-      return { ...state, text: action.text };
-    case 'SET_COLOR':
-      return { ...state, color: action.color };
-    case 'TOGGLE_GOOD':
-      return { ...state, isGood: !state.isGood };
-    default:
-      throw new Error('unHandled Action Type');
-  }
-}
+import React from 'react';
+import { useSampleDispatch, useSampleState } from './SampleContext';
 
 function ReducerSample() {
-  const [state, dispatch] = useReducer(reducer, {
-    // 예상된 타입의 값이 아닌 경우, 에러를 표시해준다
-    count: 0,
-    text: 'Hello',
-    color: 'red',
-    isGood: true,
-  });
+  const state = useSampleState();
+  const dispatch = useSampleDispatch();
 
-  // 아래처럼 선언 시 오류를 표시함
-  // const setCount = () => dispatch({ type: 'SET_COUNT'});
-  // 자동완성을 누르면, count가 필요함을 알려준다
   const setCount = () => dispatch({ type: 'SET_COUNT', count: 5 });
   const setText = () => dispatch({ type: 'SET_TEXT', text: 'Bye' });
-  const setColor = () =>
-    dispatch({ type: 'SET_COLOR', color: 'orange' });
+  const setColor = () => dispatch({ type: 'SET_COLOR', color: 'orange' });
   const toggleGood = () => dispatch({ type: 'TOGGLE_GOOD' });
 
   return (
---
+++ src/SampleContext.tsx
@@ -0,0 +1,77 @@
+import React, { createContext, Dispatch, useContext, useReducer } from 'react';
+
+type Color = 'red' | 'orange' | 'yellow';
+
+type State = {
+  count: number;
+  text: string;
+  color: Color;
+  isGood: boolean;
+};
+
+type Action =
+  | { type: 'SET_COUNT'; count: number }
+  | { type: 'SET_TEXT'; text: string }
+  | { type: 'SET_COLOR'; color: Color }
+  | { type: 'TOGGLE_GOOD' };
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'SET_COUNT':
+      return { ...state, count: action.count };
+    case 'SET_TEXT':
+      return { ...state, text: action.text };
+    case 'SET_COLOR':
+      return { ...state, color: action.color };
+    case 'TOGGLE_GOOD':
+      return { ...state, isGood: !state.isGood };
+    default:
+      throw new Error('unHandled Action Type');
+  }
+}
+
+type SampleDispatch = Dispatch<Action>;
+
+const SampleStateContext = createContext<State | null>(null);
+// React에서 import된 "Dispatch" 사용
+const SampleDispatchContext = createContext<SampleDispatch | null>(null);
+// 한 번에 제너릭을 사용해도 문제 X
+// const SampleDispatchContext = createContext<Dispatch<Action> | null>(null);
+
+type SampleProviderProps = {
+  children: React.ReactNode;
+};
+
+export function SampleProvider({ children }: SampleProviderProps) {
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    text: 'Hello',
+    color: 'red',
+    isGood: true,
+  });
+
+  return (
+    <SampleStateContext.Provider value={state}>
+      <SampleDispatchContext.Provider value={dispatch}>
+        {children}
+      </SampleDispatchContext.Provider>
+    </SampleStateContext.Provider>
+  );
+}
+
+// useContext를 사용하는 대신 사용할 커스텀 훅 생성
+export function useSampleState() {
+  const state = useContext(SampleStateContext);
+  if (!state) {
+    throw new Error('Cannot find SampleProvider: State');
+  }
+  return state;
+}
+
+export function useSampleDispatch() {
+  const dispatch = useContext(SampleDispatchContext);
+  if (!dispatch) {
+    throw new Error('Cannot find SampleProvider: dispatch');
+  }
+  return dispatch;
+}
